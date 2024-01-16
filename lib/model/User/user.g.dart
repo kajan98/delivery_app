fdafc9ee782bc5bc6686d06c42695c53026e008d
kajan98 <kajenthiran9898@gmail.com>
2024-01-16 18:36:13 +0530

    Login

---
+++ lib/model/User/user.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) => User(
+      id: json['id'] as String?,
+      name: json['name'] as String?,
+      number: json['number'] as int?,
+      password: json['password'] as String?,
+    );
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'number': instance.number,
+      'password': instance.password,
+    };
